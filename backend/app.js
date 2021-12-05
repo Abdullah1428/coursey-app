a0e16df2d3177a2002e74d654c4fb4a11ca2dd46
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2021-12-05 16:22:39 +0100

    updates for deployment on heroku

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,27 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
 
-import courseRoutes from './routes/courseRoutes.js'
+import courseRoutes from './routes/courseRoutes.js';
 
-const app = express()
+const app = express();
 
 // for parsing requests
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-// to check backend is up and running
-app.get('/', (_req, res) => {
-  res.send('API is running...')
-})
+app.use('/api', courseRoutes);
 
-app.use('/api', courseRoutes)
+// Static assets for production deployment
+if (process.env.NODE_ENV === 'production') {
+  // Define the static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 // export app
-export { app }
+export { app };
