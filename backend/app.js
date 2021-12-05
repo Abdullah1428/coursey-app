0ef80ab93e5d000bf913ab629fb9ea7e35dd1812
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2021-12-05 17:41:18 +0100

    more fixes

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/frontend/build/' + 'index.html');
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
