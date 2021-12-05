8c0a29db49bc221c9845a0ee2399d9aa528b70c6
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2021-12-05 23:00:22 +0100

    debug

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import courseRoutes from './routes/courseRoutes.js';
 
@@ -18,8 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    const path = require('path');
-    res.sendFile(path.resolve(__dirname, 'frontend' + 'build' + 'index.html'));
+    res.sendFile(path.resolve('frontend' + 'build' + 'index.html'));
   });
 }
 
