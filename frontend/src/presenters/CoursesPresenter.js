00911f729a97199a2268b464dad74a1b551bf20f
simha97 <shallak@kth.se>
2021-12-05 16:14:21 +0100

    coursesPresenter

---
+++ frontend/src/presenters/CoursesPresenter.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Courses from '../views/Courses';
+import Loader from '../components/Loader';
+
+const CoursesPresenter = () => {
+  const [courseList, setCourseList] = useState(null);
+
+  useEffect(() => {
+    const getCourseDataFromAPI = async () => {
+      let apUrl = 'http://localhost:5000/api/courses';
+
+      const { data } = await axios.get(apUrl);
+    console.log(data);
+      setCourseList(data);
+    };
+
+    getCourseDataFromAPI();
+  }, []);
+
+  return (
+    <>
+      {!courseList ? (
+        <Loader />
+      ) : (
+        <Courses courses={courseList} />
+      )}
+    </>
+  );
+};
+
+export default CoursesPresenter;
