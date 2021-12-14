4ce698c58b977a2fb323b40b13190064b6f22b02
Ayushman Khazanchi <ayushman.khazanchi@gmail.com>
2021-12-14 13:15:29 +0100

    add config file

---
+++ backend/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+export { firebaseConfig };
